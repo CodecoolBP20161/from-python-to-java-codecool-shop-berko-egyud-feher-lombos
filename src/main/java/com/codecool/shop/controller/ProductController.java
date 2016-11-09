46e4e1bab56c124cc3335b10713a7175ccf0393a
Mónika Lombos <lombos.monika@gmail.com>
2016-11-09 10:06:44 +0100

    URL's route change according to the filter (category or supplier)

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -22,20 +22,32 @@ public class ProductController {
     public static ModelAndView renderProducts(Request req, Response res) {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao productSupplierDataStore = SupplierDaoMem.getInstance();
+
         Map params = new HashMap<>();
         Integer categoryId = 1;
+        Integer supplierId = 1;
         params.put("category", new ProductCategory("All Products", "All Products", "All Products"));
 
-        if ( req.params(":id") != null ) {
-            categoryId = Integer.parseInt(req.params(":id"));
+        if ( req.params(":categoryid") != null ) {
+            categoryId = Integer.parseInt(req.params(":categoryid"));
             params.put("category", productCategoryDataStore.find(categoryId));
+        }
+        if ( req.params(":supplierid") != null ) {
+            supplierId = Integer.parseInt(req.params(":supplierid"));
+            params.put("supplier", productSupplierDataStore.find(supplierId));
         }
 
         params.put("categories", productCategoryDataStore.getAll());
         params.put("products", productDataStore.getAll());
+        params.put("suppliers", productSupplierDataStore.getAll());
 
-        if ( req.params(":id") != null ) {
+        if ( req.params(":categoryid") != null ) {
             params.put("products", productDataStore.getBy(productCategoryDataStore.find(categoryId)));
+        }
+
+        if ( req.params(":supplierid") != null ) {
+            params.put("products", productDataStore.getBy(productSupplierDataStore.find(supplierId)));
         }
 
         return new ModelAndView(params, "product/index");
