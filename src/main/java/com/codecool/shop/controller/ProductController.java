18e3ceb4cc61ae425b0cfa5f49818a5ba3bdca38
JacketWhite <feherkeee@gmail.com>
2016-11-09 17:34:38 +0100

    session storage for cart

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -29,6 +29,8 @@ public class ProductController {
         Map params = new HashMap<>();
         params.put("category", productCategoryDataStore.find(1));
         params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        Order cart = req.session().attribute("Cart");
+        params.put("cart", cart);
         return new ModelAndView(params, "product/index");
     }
 
