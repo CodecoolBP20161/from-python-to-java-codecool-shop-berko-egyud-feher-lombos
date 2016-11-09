ad1a50e2373db7418922bd5d7589e170c02e2337
JacketWhite <feherkeee@gmail.com>
2016-11-09 11:39:29 +0100

    transforms order into representable lineitem

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -3,5 +3,24 @@ package com.codecool.shop.model;
 /**
  * Created by david on 11/8/16.
  */
-public class LineItem {
+public class LineItem extends Product {
+    private int quantity;
+
+    public LineItem(Product product) {
+        this.setName(product.getName());
+        this.setDefaultPrice(product.getDefaultPrice());
+        this.setDefaultCurrency(product.getDefaultCurrency());
+        this.setDescription(product.getDescription());
+        this.setProductCategory(product.getProductCategory());
+        this.setSupplier(product.getSupplier());
+        this.quantity = 1;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
