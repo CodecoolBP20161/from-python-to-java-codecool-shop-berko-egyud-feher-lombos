e9486baa4f317a4b988e65bf08892fc85e81ee15
Mónika Lombos <lombos.monika@gmail.com>
2016-11-10 16:52:13 +0100

    Example data are outsourced

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import com.codecool.shop.controller.ProductController;
 import com.codecool.shop.dao.*;
 import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.model.*;
+import com.codecool.shop.testdata.TestData;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
@@ -15,7 +16,7 @@ public class Main {
         staticFileLocation("/public");
         port(8888);
 
-        populateData();
+        TestData.populateData();
 
         get("/add/:id", ProductController::addToCart);
         get("/", ProductController::renderProducts, new ThymeleafTemplateEngine());
@@ -24,37 +25,5 @@ public class Main {
         get("/supplier/:supplierid", ProductController::renderProducts, new ThymeleafTemplateEngine());
         get("/cartcontent", ProductController::renderCartContent, new ThymeleafTemplateEngine());
     }
-
-    public static void populateData() {
-
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-
-        //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
-        Supplier samsung = new Supplier("Samsung", "Digital content and services");
-        supplierDataStore.add(samsung);
-        Supplier lg = new Supplier("LG", "Digital content and services");
-        supplierDataStore.add(lg);
-
-        //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
-        ProductCategory tv = new ProductCategory("TV", "Hardware", "TV is a telecommunication medium used for transmitting moving images in monochrome (black-and-white), or in color");
-        productCategoryDataStore.add(tv);
-
-        //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Samsung 32J5200 LED", 350.9f, "USD", "With ConnectShare Movie, simply plug your USB memory drive or HDD into the TV and instantaneously enjoy movies, photos or music.", tv, samsung));
-        productDataStore.add(new Product("LG 55EG910V OLED", 500.6f, "USD", "With ConnectShare Movie, simply plug your USB memory drive or HDD into the TV and instantaneously enjoy movies, photos or music.", tv, lg));
-
-    }
-
 
 }
