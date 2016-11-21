5a49c6b3d9f13c3bad8f7d96c3ccf29d54378f8b
JacketWhite <feherkeee@gmail.com>
2016-11-21 23:35:23 +0100

    dynamically sets the db properties based on connection.properties

--- src/main/java/com/codecool/shop/dao/implementation/db/ProductCategoryDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/db/ProductCategoryDaoDB.java
@@ -3,10 +3,19 @@ package com.codecool.shop.dao.implementation.db;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.services.ConnectionPropertyValues;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class ProductCategoryDaoDB implements ProductCategoryDao {
+    private static ConnectionPropertyValues configReader = new ConnectionPropertyValues();
+    private static HashMap DBprops = configReader.getPropValues();
+
+    private static final String DATABASE = "jdbc:postgresql://" + DBprops.get("url") + "/" + DBprops.get("database");
+    private static final String DB_USER = String.valueOf(DBprops.get("user"));
+    private static final String DB_PASSWORD = String.valueOf(DBprops.get("password"));
+
     @Override
     public void add(ProductCategory category) {
 
--- src/main/java/com/codecool/shop/dao/implementation/db/ProductDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/db/ProductDaoDB.java
@@ -5,10 +5,19 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
+import com.codecool.shop.services.ConnectionPropertyValues;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class ProductDaoDB implements ProductDao {
+    private static ConnectionPropertyValues configReader = new ConnectionPropertyValues();
+    private static HashMap DBprops = configReader.getPropValues();
+
+    private static final String DATABASE = "jdbc:postgresql://" + DBprops.get("url") + "/" + DBprops.get("database");
+    private static final String DB_USER = String.valueOf(DBprops.get("user"));
+    private static final String DB_PASSWORD = String.valueOf(DBprops.get("password"));
+
     @Override
     public void add(Product product) {
 
