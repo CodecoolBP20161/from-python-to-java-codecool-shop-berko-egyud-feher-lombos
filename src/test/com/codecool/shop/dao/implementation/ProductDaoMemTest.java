b35923b09aaaaf25a9322c5aa7acb58e5c52d8c3
egyud_v <vivien.egyud@gmail.com>
2016-11-22 11:41:45 +0100

    Created add_Product_To_ProductDaoMem() testing method

--- src/test/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
+++ src/test/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
@@ -1,17 +1,35 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class ProductDaoMemTest {
-    @Test
-    public void getInstance() throws Exception {
 
+    ProductDao productDao;
+
+    @Mock
+    Product product;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        product = new Product();
+        productDao = ProductDaoMem.getInstance();
     }
 
-    @Test
-    public void add() throws Exception {
 
+    @Test
+    public void add_Product_To_ProductDaoMem() throws Exception {
+        productDao.add(product);
+        assertEquals(product.getId(), productDao.getAll().get(0).getId());
     }
 
     @Test
@@ -37,6 +55,12 @@ public class ProductDaoMemTest {
     @Test
     public void getBy1() throws Exception {
 
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        product = null;
+        productDao = null;
     }
 
 }
