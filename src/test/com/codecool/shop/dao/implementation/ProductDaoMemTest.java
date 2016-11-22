487c575570df41407257179265eac788c02bc1dd
Mónika Lombos <lombos.monika@gmail.com>
2016-11-22 17:26:28 +0100

    Create getAll_Should_Return_ProductList testing method

--- src/test/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
+++ src/test/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
@@ -1,6 +1,5 @@
 package com.codecool.shop.dao.implementation;
 
-import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.model.Product;
 import org.junit.After;
 import org.junit.Before;
@@ -8,12 +7,14 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 
 public class ProductDaoMemTest {
 
-    ProductDao productDao;
+    ProductDaoMem productDaoMem;
 
     @Mock
     Product product = new Product();
@@ -22,42 +23,36 @@ public class ProductDaoMemTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        productDao = ProductDaoMem.getInstance();
+        productDaoMem = ProductDaoMem.getInstance();
         System.out.println("Setting up...");
     }
 
 
     @Test
     public void add_Product_To_ProductDaoMem() throws Exception {
-        productDao.add(product);
-        assertEquals(product.getId(), productDao.getAll().get(0).getId());
+        productDaoMem.add(product);
+        assertEquals(product.getId(), productDaoMem.getAll().get(0).getId());
         System.out.println("Test add_Product_To_ProductDaoMem passed ...");
     }
 
     @Test
     public void find_Should_Return_Product() throws Exception {
-        productDao.add(product);
-        assertEquals(product.toString(), productDao.find(product.getId()).toString());
+        productDaoMem.add(product);
+        assertEquals(product.toString(), productDaoMem.find(product.getId()).toString());
         System.out.println("Test find_Should_Return_Product passed ...");
     }
 
     @Test
     public void find_Should_Return_Null() throws Exception {
-        assertEquals(null, productDao.find(product.getId()));
+        assertEquals(null, productDaoMem.find(product.getId()));
         System.out.println("Test find_Should_Return_Null passed ...");
     }
 
     @Test
-    public void remove_Product_From_ProductDao() throws Exception {
-        productDao.add(product);
-        productDao.remove(product.getId());
-        assertEquals(null, productDao.find(product.getId()));
-        System.out.println("Test remove_Product_From_ProductDao passed ...");
-    }
-
-    @Test
-    public void getAll() throws Exception {
-
+    public void getAll_Should_Return_ProductList() throws Exception {
+        productDaoMem.add(product);
+        assertEquals(Arrays.asList(product), productDaoMem.getAll());
+        System.out.println("Test getAll_Should_Return_ProductList passed ...");
     }
 
     @Test
@@ -72,8 +67,8 @@ public class ProductDaoMemTest {
 
     @After
     public void tearDown() throws Exception {
+        productDaoMem.remove(product.getId());
         product = null;
-        productDao = null;
         System.out.println("Tearing down to cleaning garbage collection");
     }
 
