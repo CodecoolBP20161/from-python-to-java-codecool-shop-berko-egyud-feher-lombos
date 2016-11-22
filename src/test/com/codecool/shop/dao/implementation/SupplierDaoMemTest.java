db1a7d17c516e5021e8f5448fb862befa6592ed2
egyud_v <vivien.egyud@gmail.com>
2016-11-22 11:59:18 +0100

    Created add_Supplier_To_SupplierDaoMem() testing method

--- src/test/com/codecool/shop/dao/implementation/SupplierDaoMemTest.java
+++ src/test/com/codecool/shop/dao/implementation/SupplierDaoMemTest.java
@@ -1,17 +1,34 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Supplier;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class SupplierDaoMemTest {
-    @Test
-    public void getInstance() throws Exception {
 
+    SupplierDao supplierDao;
+
+    @Mock
+    Supplier supplier;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        supplier = new Supplier("name", "description");
+        supplierDao = SupplierDaoMem.getInstance();
     }
 
     @Test
-    public void add() throws Exception {
-
+    public void add_Supplier_To_SupplierDaoMem() throws Exception {
+        supplierDao.add(supplier);
+        assertEquals(supplier.getId(), supplierDao.getAll().get(0).getId());
     }
 
     @Test
@@ -29,4 +46,9 @@ public class SupplierDaoMemTest {
 
     }
 
+    @After
+    public void tearDown() throws Exception {
+        supplier = null;
+        supplierDao = null;
+    }
 }
