9d7af9164e3f5e0769882065b00537cfa49a2ba9
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-11-24 00:12:34 +0100

    getAll implemented

--- src/main/java/com/codecool/shop/dao/implementation/db/OrderDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/db/OrderDaoDB.java
@@ -3,6 +3,7 @@ package com.codecool.shop.dao.implementation.db;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.model.LineItem;
 import com.codecool.shop.model.Order;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.Status;
 import javassist.NotFoundException;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
@@ -17,15 +18,17 @@ public class OrderDaoDB extends AbstractDBHandler implements OrderDao{
 
     @Override
     public void add(Order order) {
+
         try {
+
             PreparedStatement stmt;
             stmt = getConnection().prepareStatement("INSERT INTO \"order\" (STATUS, TOTAL_PRICE) VALUES (?, ?)");
             stmt.setString(1, order.getStatus().toString());
             stmt.setDouble(2, order.getTotalPrice());
             stmt.executeQuery();
+
         } catch (Exception e) {
             e.printStackTrace();
-
         }
     }
 
@@ -40,37 +43,65 @@ public class OrderDaoDB extends AbstractDBHandler implements OrderDao{
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
         ){
+
             if (resultSet.next()) {
 
                 order = new Order(Status.valueOf(resultSet.getString("STATUS")), resultSet.getInt("id"));
                 List<LineItem> lineItems = lineItemDB.getBy(order.getId());
-                // For each lineItem, add a product times the quantity, to the order.
-                // Maybe we should implement a method in order which adds lineItem, and not product, we should ask Thommyboy
+
+                // For each lineItem, add a product times the quantity to the order.
                 for (LineItem lineItem : lineItems) {
-                    for(int quantity = 0; quantity < lineItem.getQuantity(); quantity++) {
-                        order.add(productDB.find(lineItem.getProductId()));
-                    }
+                    Product parentProduct = productDB.find(lineItem.getProductId());
+                    for(int i = 0; i < lineItem.getQuantity(); i++) order.add(parentProduct);
+
                 }
                 return order;
-
             } else {
                 throw new NotFoundException("Order not found");
             }
         } catch (SQLException e) {
-        e.printStackTrace();
+            e.printStackTrace();
         }
         return null;
     }
 
     @Override
     public void remove(int id) {
-        String query = "DELETE FROM \"order\" WHERE ID = '" + id +"';";
+        String query = "DELETE FROM \"order\" WHERE ID = '" + id + "';";
         executeQuery(query);
     }
 
     @Override
-    public List<Order> getAll() {
-        throw new NotImplementedException();
+    public List<Order> getAll() throws NotFoundException {
+        Order order;
+        LineItemDaoDB lineItemDB = new LineItemDaoDB();
+        ProductDaoDB productDB = new ProductDaoDB();
+        List<Order> resultList = new ArrayList<>();
+        String query = "SELECT * FROM \"order\";";
+
+        try(Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+        ){
+            while (resultSet.next()) {
+
+                order = new Order(Status.valueOf(resultSet.getString("STATUS")), resultSet.getInt("id"));
+                List<LineItem> lineItems = lineItemDB.getBy(order.getId());
+
+                // For each lineItem, add a product times the quantity to the order.
+                for (LineItem lineItem : lineItems) {
+                    Product parentProduct = productDB.find(lineItem.getProductId());
+                    for (int i = 0; i < lineItem.getQuantity(); i++) order.add(parentProduct);
+                    resultList.add(order);
+                }
+            }
+            return resultList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
