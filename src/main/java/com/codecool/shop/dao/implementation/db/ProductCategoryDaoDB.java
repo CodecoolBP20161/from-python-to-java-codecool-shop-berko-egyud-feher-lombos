9b0cd8ce88eba6225de5a7f0a7d680f305adce01
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-11-23 20:28:34 +0100

    only  getAll has to be implemented, other methods are ready

--- src/main/java/com/codecool/shop/dao/implementation/db/ProductCategoryDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/db/ProductCategoryDaoDB.java
@@ -1,15 +1,10 @@
 package com.codecool.shop.dao.implementation.db;
 
-
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
-import com.codecool.shop.services.ConnectionPropertyValues;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.HashMap;
+import javassist.NotFoundException;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.sql.*;
 import java.util.List;
 
 public class ProductCategoryDaoDB extends AbstractDBHandler implements ProductCategoryDao {
@@ -18,32 +13,49 @@ public class ProductCategoryDaoDB extends AbstractDBHandler implements ProductCa
     public void add(ProductCategory category) {
         try {
             PreparedStatement stmt;
-            stmt = getConnection().prepareStatement("INSERT INTO \"category\" VALUES (?, ?, ?, ?)");
-            stmt.setString(1, Integer.toString(category.getId()));
-            stmt.setString(2, category.getName());
-            stmt.setString(3, category.getDescription());
-            stmt.setString(4, category.getDepartment());
+            stmt = getConnection().prepareStatement("INSERT INTO \"category\" (\"NAME\", DESCRIPTION, DEPARTMENT) VALUES ( ?, ?, ?)");
+            stmt.setString(1, category.getName());
+            stmt.setString(2, category.getDescription());
+            stmt.setString(3, category.getDepartment());
             stmt.executeQuery();
         } catch (Exception e) {
             e.printStackTrace();
-
         }
     }
 
     @Override
-    public ProductCategory find(int id) {
+    public ProductCategory find(int id) throws NotFoundException {
+        ProductCategory category;
+        ProductDaoDB productDB = new ProductDaoDB();
+        String query = "SELECT * FROM category WHERE ID = '" + id + "';";
+
+        try(Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query)
+        ){
+            if (resultSet.next()) {
+                category = new ProductCategory(resultSet.getInt("ID"),
+                                      resultSet.getString("NAME"),
+                                      resultSet.getString("DESCRIPTION"),
+                                      resultSet.getString("DEPARTMENT"));
+                productDB.getBy(category).forEach(category::addProduct);
+                return category;
+            } else {
+                throw new NotFoundException("Product not found");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
-    public void remove(int id) {
-        String query = "DELETE FROM \"category\" WHERE id = '" + id +"';";
+    public void remove(ProductCategory category) {
+        String query = "DELETE FROM \"category\" WHERE id = '" + category.getId() +"';";
         executeQuery(query);
     }
 
     @Override
-    public List<ProductCategory> getAll() {
-        return null;
-    }
+    public List<ProductCategory> getAll() {throw new NotImplementedException();}
 
 }
