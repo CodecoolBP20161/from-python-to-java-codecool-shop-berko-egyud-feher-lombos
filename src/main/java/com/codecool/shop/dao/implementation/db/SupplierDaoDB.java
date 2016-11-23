ef6c7ece882cc9a0540349d3be3b3ac6e4ae4a84
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-11-23 20:26:59 +0100

    only find + getAll have to be implemented, other methods are ready

--- src/main/java/com/codecool/shop/dao/implementation/db/SupplierDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/db/SupplierDaoDB.java
@@ -1,9 +1,9 @@
 package com.codecool.shop.dao.implementation.db;
 
-
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 import com.codecool.shop.services.ConnectionPropertyValues;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.sql.*;
 import java.util.HashMap;
@@ -15,10 +15,9 @@ public class SupplierDaoDB extends AbstractDBHandler implements SupplierDao {
     public void add(Supplier supplier) {
             try {
                 PreparedStatement stmt;
-                stmt = getConnection().prepareStatement("INSERT INTO \"supplier\" VALUES (?, ?, ?)");
-                stmt.setInt(1, supplier.getId());
-                stmt.setString(2, supplier.getName());
-                stmt.setString(3, supplier.getDescription());
+                stmt = getConnection().prepareStatement("INSERT INTO \"supplier\" (\"NAME\", DESCRIPTION) VALUES (?, ?)");
+                stmt.setString(1, supplier.getName());
+                stmt.setString(2, supplier.getDescription());
                 stmt.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
@@ -27,18 +26,18 @@ public class SupplierDaoDB extends AbstractDBHandler implements SupplierDao {
 
         @Override
         public Supplier find ( int id){
+            //not ready yet!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Supplier supplier;
+            //String query = "SELECT * FROM supplier WHERE ID='" + id + "';";
             return null;
         }
 
         @Override
-        public void remove ( int id){
-            String query = "DELETE FROM supplier WHERE id = '" + id +"';";
+        public void remove ( Supplier supplier){
+            String query = "DELETE FROM supplier WHERE id = '" + supplier.getId() +"';";
             executeQuery(query);
         }
 
         @Override
-        public List<Supplier> getAll () {
-
-            return null;
-        }
+        public List<Supplier> getAll () {throw new NotImplementedException();}
 }
