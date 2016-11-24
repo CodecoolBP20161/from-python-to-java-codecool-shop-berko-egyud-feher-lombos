f03e121c22b377e443414b5b02c5e0599f23687c
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-11-24 09:21:39 +0100

    added exception handling

--- src/main/java/com/codecool/shop/dao/SupplierDao.java
+++ src/main/java/com/codecool/shop/dao/SupplierDao.java
@@ -1,13 +1,14 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.model.Supplier;
+import javassist.NotFoundException;
 
 import java.util.List;
 
 public interface SupplierDao {
 
     void add(Supplier category);
-    Supplier find(int id);
+    Supplier find(int id) throws NotFoundException;
     void remove(int id);
 
     List<Supplier> getAll();
