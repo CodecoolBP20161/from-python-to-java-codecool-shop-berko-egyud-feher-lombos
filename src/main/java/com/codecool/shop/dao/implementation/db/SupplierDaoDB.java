cb057228fcfd318a71e0405c907fc143e7a8475a
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-11-24 09:44:01 +0100

    getAll() implemented

--- src/main/java/com/codecool/shop/dao/implementation/db/SupplierDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/db/SupplierDaoDB.java
@@ -1,12 +1,14 @@
 package com.codecool.shop.dao.implementation.db;
 
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
-import com.codecool.shop.services.ConnectionPropertyValues;
+import javassist.NotFoundException;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.sql.*;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SupplierDaoDB extends AbstractDBHandler implements SupplierDao {
@@ -25,10 +27,29 @@ public class SupplierDaoDB extends AbstractDBHandler implements SupplierDao {
     }
 
         @Override
-        public Supplier find ( int id){
-            //not ready yet!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        public Supplier find ( int id) throws NotFoundException {
             Supplier supplier;
-            //String query = "SELECT * FROM supplier WHERE ID='" + id + "';";
+            String query = "SELECT * FROM supplier WHERE ID='" + id + "';";
+
+            try (Connection connection = getConnection();
+                 Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(query);
+            ) {
+                if (resultSet.next()) {
+
+                    ProductDaoDB productDB = new ProductDaoDB();
+                    supplier = new Supplier(resultSet.getString("NAME"), resultSet.getString("DESCRIPTION"));
+
+                    // Iterating through the products queried by supplier, and adding them to the suppliers 'products' field
+                    productDB.getBy(supplier).forEach(supplier::addProduct);
+                    return supplier;
+
+                } else {
+                    throw new NotFoundException("Supplier not found");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             return null;
         }
 
@@ -39,5 +60,31 @@ public class SupplierDaoDB extends AbstractDBHandler implements SupplierDao {
         }
 
         @Override
-        public List<Supplier> getAll () {throw new NotImplementedException();}
+        public List<Supplier> getAll () throws NotFoundException {
+            ProductDaoDB productDB = new ProductDaoDB();
+            List<Supplier> resultList = new ArrayList<>();
+            String query = "SELECT * FROM supplier;";
+
+
+            try(Connection connection = getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query)
+
+            ){
+                while (resultSet.next()) {
+
+                    Supplier supplier = new Supplier(
+                            resultSet.getInt("ID"),
+                            resultSet.getString("NAME"),
+                            resultSet.getString("DESCRIPTION"));
+
+                    productDB.getBy(supplier).forEach(supplier::addProduct);
+                    resultList.add(supplier);
+                }
+                return resultList;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 }
