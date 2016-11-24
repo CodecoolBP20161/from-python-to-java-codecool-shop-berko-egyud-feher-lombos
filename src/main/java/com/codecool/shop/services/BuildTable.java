b3d078b9d3018a0e3464d48ac2ce9ab24e053f49
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2016-11-24 12:32:02 +0100

    SQL errors corrected

--- src/main/java/com/codecool/shop/services/BuildTable.java
+++ src/main/java/com/codecool/shop/services/BuildTable.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import java.util.HashMap;
 
 public class BuildTable {
-    public static void main(String[] args) {
+    public static void build() {
          ConnectionPropertyValues configReader = new ConnectionPropertyValues();
          HashMap DBprops = configReader.getPropValues();
 
@@ -19,50 +19,50 @@ public class BuildTable {
         try (Connection connection = DriverManager.getConnection(DATABASE, DB_USER, DB_PASSWORD)){
             Statement statement = connection.createStatement();
 
-            String createProduct =  "DROP TABLE IF EXISTS product;" +
+            String createProduct =  "DROP TABLE IF EXISTS  product CASCADE;" +
                                     "CREATE TABLE product" +
                                     "(" +
-                                    "ID SERIAL PRIMARY KEY ON DELETE CASCADE," +
+                                    "ID SERIAL PRIMARY KEY," +
                                     "NAME varchar(255)," +
                                     "DESCRIPTION varchar(255)," +
                                     "PRICE varchar(255)," +
                                     "CURRENCY varchar(255)," +
-                                    "PRODUCT_CATEGORY int references category(ID)," +
-                                    "PRODUCT_SUPPLIER int references supplier(ID)" +
+                                    "PRODUCT_CATEGORY int references category(ID) ON DELETE CASCADE," +
+                                    "PRODUCT_SUPPLIER int references supplier(ID) ON DELETE CASCADE" +
                                     ");";
 
-            String createCategory = "DROP TABLE IF EXISTS category;" +
+            String createCategory = "DROP TABLE IF EXISTS category CASCADE;" +
                                     "CREATE TABLE category" +
                                     "(" +
-                                    "ID SERIAL PRIMARY KEY ON DELETE CASCADE," +
+                                    "ID SERIAL PRIMARY KEY," +
                                     "NAME varchar(255)," +
                                     "DESCRIPTION varchar(255)," +
                                     "DEPARTMENT varchar(255)" +
                                     ");";
 
-            String createOrder = "DROP TABLE IF EXISTS \"order\";" +
+            String createOrder = "DROP TABLE IF EXISTS \"order\" CASCADE;" +
                                  "CREATE TABLE \"order\"" +
                                  "(" +
-                                 "ID SERIAL PRIMARY KEY ON DELETE CASCADE," +
+                                 "ID SERIAL PRIMARY KEY," +
                                  "STATUS varchar(255)," +
                                  "TOTAL_PRICE varchar(255)" +
                                  ");";
 
-            String createSupplier = "DROP TABLE IF EXISTS supplier;" +
+            String createSupplier = "DROP TABLE IF EXISTS supplier CASCADE;" +
                                     "CREATE TABLE supplier" +
                                     "(" +
-                                    "ID SERIAL PRIMARY KEY ON DELETE CASCADE," +
+                                    "ID SERIAL PRIMARY KEY," +
                                     "NAME varchar(255)," +
                                     "DESCRIPTION varchar(255)" +
                                     ");";
 
-            String createLineItem = "DROP TABLE IF EXISTS lineitem;" +
+            String createLineItem = "DROP TABLE IF EXISTS lineitem CASCADE;" +
                                     "CREATE TABLE lineitem" +
                                     "(" +
                                     "ID SERIAL PRIMARY KEY," +
                                     "QUANTITY int," +
-                                    "PRODUCT int REFERENCES product(ID)," +
-                                    "\"ORDER\" int REFERENCES \"order\"(ID)" +
+                                    "PRODUCT int REFERENCES product(ID) ON DELETE CASCADE," +
+                                    "\"ORDER\" int REFERENCES \"order\"(ID) ON DELETE CASCADE" +
                                     ");";
         statement.execute(createSupplier + createCategory + createOrder + createProduct + createLineItem);
 
