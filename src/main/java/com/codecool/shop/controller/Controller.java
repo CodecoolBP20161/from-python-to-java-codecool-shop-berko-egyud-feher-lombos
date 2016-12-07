c118a7f5bd5e6b8766273ccd242c275b07423b7d
Mónika Lombos <lombos.monika@gmail.com>
2016-12-07 22:11:52 +0100

    Removed unnecessary rows

--- src/main/java/com/codecool/shop/controller/Controller.java
+++ src/main/java/com/codecool/shop/controller/Controller.java
@@ -5,11 +5,9 @@ import com.codecool.shop.dao.implementation.db.ProductCategoryDaoDB;
 import com.codecool.shop.dao.implementation.db.ProductDaoDB;
 import com.codecool.shop.dao.implementation.db.SupplierDaoDB;
 import com.codecool.shop.model.Order;
-import com.codecool.shop.model.Orderable;
 import com.codecool.shop.model.ProductCategory;
 import javassist.NotFoundException;
 import spark.Request;
-import spark.Response;
 
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -54,45 +52,5 @@ public class Controller {
         Order cart = req.session().attribute("Cart");
         params.put("cart", cart);
         return params;
-
-    }
-
-
-    // Handle the content of the session and set the variables of Order object
-    public static String addToCart(Request req, Response res) throws NotFoundException {
-        int id = Integer.parseInt(req.params(":id"));
-        Orderable order;
-
-        if (req.session().attribute("Cart") == null) {
-            order = new Order();
-        } else {
-            order = req.session().attribute("Cart");
-        }
-
-        order.add(ProductDB.find(id));
-        req.session().attribute("Cart", order);
-        res.redirect(req.session().attribute("currentUrl"));
-        return null;
-    }
-
-    // Handle the content of the session and set the variables of Order object
-    public static String removeFromCart(Request req, Response res) throws NotFoundException {
-        int id = Integer.parseInt(req.params(":id"));
-        Orderable order;
-        order = req.session().attribute("Cart");
-
-        // remove one product from session
-        order.remove(ProductDB.find(id));
-        req.session().attribute("Cart", order);
-        res.redirect(req.session().attribute("currentUrl"));
-        return null;
-    }
-
-    // Handle the content of the session and set the variables of Order object
-    public static String removeAllFromCart(Request req, Response res) throws NotFoundException {
-        Order order = null;
-        req.session().attribute("Cart", order);
-        res.redirect(req.session().attribute("currentUrl"));
-        return null;
     }
 }
