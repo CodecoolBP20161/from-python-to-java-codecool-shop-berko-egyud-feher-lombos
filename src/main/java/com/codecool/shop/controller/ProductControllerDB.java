fb9178783c74d2e922e9fd983f126240efeec5eb
Mónika Lombos <lombos.monika@gmail.com>
2016-12-08 07:41:12 +0100

    Index page operate well with pagination, but the pagination buttons didn't disappear on filtered product page.

--- src/main/java/com/codecool/shop/controller/ProductControllerDB.java
+++ src/main/java/com/codecool/shop/controller/ProductControllerDB.java
@@ -1,7 +1,9 @@
 package com.codecool.shop.controller;
 
 
-import com.codecool.shop.dao.implementation.db.*;
+import com.codecool.shop.dao.implementation.db.ProductCategoryDaoDB;
+import com.codecool.shop.dao.implementation.db.ProductDaoDB;
+import com.codecool.shop.dao.implementation.db.SupplierDaoDB;
 import javassist.NotFoundException;
 import spark.ModelAndView;
 import spark.Request;
@@ -14,19 +16,42 @@ import java.util.Map;
 public class ProductControllerDB {
 
     private static ProductCategoryDaoDB ProductCategoryDB = new ProductCategoryDaoDB();
+    private static ProductDaoDB ProductDB = new ProductDaoDB();
     private static SupplierDaoDB supplierDB = new SupplierDaoDB();
 
     static Integer categoryId = 1;
     static Integer supplierId = 1;
 
-    // Action for display all or filtered products
+    // Action for display all
     public static ModelAndView renderProducts(Request req, Response res) throws NotFoundException, SQLException {
         Map params = Controller.setParams(req);
 
+        // products put to params according to the paginationNumber
+        if (req.queryParams("paginationNumber") == null) {
+            params.put("products", ProductDB.getProductByPagination(1));
+        } else if (req.queryParams("paginationNumber") != null) {
+            int paginationNumber = Integer.parseInt(req.queryParams("paginationNumber"));
+            if (paginationNumber == 1) {
+                params.put("products", ProductDB.getProductByPagination(1));
+            } else if (paginationNumber == 2) {
+                params.put("products", ProductDB.getProductByPagination(11));
+            } else if (paginationNumber == 3) {
+                params.put("products", ProductDB.getProductByPagination(21));
+            }
+        }
+        return new ModelAndView(params, "product/index");
+    }
+
+    // Action for display filtered products
+    public static ModelAndView renderFilteredProducts(Request req, Response res) throws NotFoundException, SQLException {
+        Map params = Controller.setParams(req);
+
         if ( req.params(":categoryid") != null ) {
+            params = Controller.setParams(req);
             params.put("category", ProductCategoryDB.find(categoryId));
         }
         if ( req.params(":supplierid") != null ) {
+            params = Controller.setParams(req);
             params.put("supplier", supplierDB.find(supplierId));
         }
         return new ModelAndView(params, "product/index");
