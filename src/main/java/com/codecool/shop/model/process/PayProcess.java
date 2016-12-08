2638ed0fcda1a81982694ffad412cc6aa07b17e5
Mónika Lombos <lombos.monika@gmail.com>
2016-12-08 23:29:57 +0100

    Email content expanded

--- src/main/java/com/codecool/shop/model/process/PayProcess.java
+++ src/main/java/com/codecool/shop/model/process/PayProcess.java
@@ -10,7 +10,7 @@ import javassist.NotFoundException;
 
 public class PayProcess extends AbstractProcess {
 
-    private  ShippingDataDB shippingDataDB = ShippingDataDB.getInstance();
+    private ShippingDataDB shippingDataDB = ShippingDataDB.getInstance();
     private OrderDaoDB orderDaoDB = OrderDaoDB.getInstance();
 
     @Override
@@ -24,9 +24,21 @@ public class PayProcess extends AbstractProcess {
     @Override
     public void stepAfter(Orderable item) throws NotFoundException {
 
-        String userEmail = shippingDataDB.find(((Order) item).getId());
-        System.out.println(userEmail);
-        EmailSender.send(userEmail, "please", "please");
+        String userEmail = shippingDataDB.find(((Order) item).getId()).get(0);
+        String userFirstName = shippingDataDB.find(((Order) item).getId()).get(1);
+        String userLastName = shippingDataDB.find(((Order) item).getId()).get(2);
+        String subject = "Your " + ((Order) item).getId() + " ID  Codecool.com Order";
+        String message = "Hello " + userFirstName + " " + userLastName+ " ,\n\n\n" +
+                "Thanks for your order. We’ll let you know once your item(s) have dispatched.\n " +
+                "Should you need to contact us for any reason, please know that we can give out\n " +
+                "order information only to the name and e-mail address associated with your\n " +
+                "account. \n\n Thank you again for shopping with us. You can view the status \n" +
+                "of your order or make changes to it by visiting Your orders on Codecool.com. \n\n\n" +
+                "Order Details:\n" +
+                "Ordered items: " + ((Order) item).getItemsToBuy() + "\n " +
+                "Total Price: " + ((Order) item).getTotalPrice() + " USD";
+
+        EmailSender.send(userEmail, subject, message);
 
         System.out.println("stepAfter method...");
     }
