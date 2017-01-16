49dd53f7aa38822eb507818974c87b5421d224be
lombocska <lombos.monika@gmail.com>
2017-01-16 18:04:16 +0100

    Added skeleton of postal fee microservice handling

---
+++ src/main/java/com/codecool/shop/controller/postal_fee_controller/PostalFeeCalculatorController.java
@@ -0,0 +1,35 @@
+package com.codecool.shop.controller.postal_fee_controller;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.utils.URIBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
+
+/**
+ * Created by lombocska on 2017. 01. 16..
+ */
+public class PostalFeeCalculatorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PostalFeeCalculatorController .class);
+    private static final String SERVICE_URL = "http://localhost:60000/";
+
+    public static String getPostalFee(){
+
+        return null;
+    }
+
+    private static String execute(String url) throws IOException, URISyntaxException {
+        //URI uri = new URIBuilder(SERVICE_URL + url).build();
+        //return Request.Get(uri).execute().returnContent().asString();
+        return null;
+    }
+}
---
+++ src/main/java/com/codecool/shop/controller/postal_fee_controller/PostalFeeCalculatorServiceController.java
@@ -0,0 +1,7 @@
+package com.codecool.shop.controller.postal_fee_controller;
+
+/**
+ * Created by lombocska on 2017. 01. 16..
+ */
+public class PostalFeeCalculatorServiceController {
+}
