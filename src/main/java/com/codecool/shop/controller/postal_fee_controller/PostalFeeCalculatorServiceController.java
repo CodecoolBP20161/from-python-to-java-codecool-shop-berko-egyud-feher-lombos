32e9931f1511b520c652a4d4e6c1850fb7c986d1
lombocska <lombos.monika@gmail.com>
2017-01-17 10:42:37 +0100

    Added the "reading from database(shippingtable)" part & it operate well :panda_face:

--- src/main/java/com/codecool/shop/controller/postal_fee_controller/PostalFeeCalculatorServiceController.java
+++ src/main/java/com/codecool/shop/controller/postal_fee_controller/PostalFeeCalculatorServiceController.java
@@ -1,5 +1,9 @@
 package com.codecool.shop.controller.postal_fee_controller;
 
+import com.codecool.shop.dao.implementation.db.ShippingDataDB;
+import com.codecool.shop.model.Order;
+import com.codecool.shop.model.Orderable;
+import javassist.NotFoundException;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
@@ -10,30 +14,49 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-/**
- * Created by lombocska on 2017. 01. 16..
- */
+
 public class PostalFeeCalculatorServiceController {
 
     private static final Logger logger = LoggerFactory.getLogger(PostalFeeCalculatorServiceController .class);
     private static final String SERVICE_URL = "http://localhost:60000";
     private static final String TARGET_PARAM_KEY = "target";
-    private static final String WEBSHOP_PARAM_KEY="webshop";
+    private static final String WEBSHOP_PARAM_KEY= "webshop";
 
-
-    public static String getPostalFee(spark.Request request) throws IOException, URISyntaxException {
+    /**
+     * <h1>Get two city and build an API URL to get the postal fee from the postal_fee_calculator_microservice.
+     * One of the city is the userCity, what we get from the database and the other is the webshopCity, that is permanent.
+     * </h1>
+     * @param request
+     * @param order
+     * @return Built URL as a String
+     * @throws IOException
+     * @throws URISyntaxException
+     * @throws NotFoundException
+     * @author Vivi and Moni
+     * @version final
+     */
+    public static String getPostalFee(spark.Request request, Order order) throws IOException, URISyntaxException, NotFoundException {
         logger.info("Getting Postal Fee...");
 
-        // TODO: get the userCity from shippingdata' table instead of this userCity
-        String userCity = "Budapest";
-        String webshopCity = "Polgardi";
+        String webshopCity = "Budapest";
+
+        ShippingDataDB shippingDataDB = ShippingDataDB.getInstance();
+        String userCity = shippingDataDB.find(order.getId()).get(3);
         URIBuilder builder = new URIBuilder(SERVICE_URL + "/api");
         builder.addParameter(TARGET_PARAM_KEY, userCity);
         builder.addParameter(WEBSHOP_PARAM_KEY, webshopCity);
-        logger.warn("PROBA" + builder.build());
+
+        logger.info("Getting URL" + builder.build());
+
         return execute(builder.build());
     }
 
+    /**
+     *<h1>Execute get request with the built URL and return the response.</h1>
+     * @param uri
+     * @return the response
+     * @throws IOException
+     */
     private static String execute(URI uri) throws IOException {
         return Request.Get(uri)
                 .execute()
